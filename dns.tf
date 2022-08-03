6353a8079ef2f09a03957bebb8172f18a17a9e7f
Callum <callumccr@gmail.com>
2022-08-03 14:46:13 +0100

    fix: resoled aws_route_record fixing how records are created

--- dns.tf
+++ dns.tf
@@ -22,8 +22,8 @@ resource "aws_route53_record" "dns" {
 
   alias {
     evaluate_target_health = true
-    name                   = aws_api_gateway_domain_name.dns[each.value].cloudfront_domain_name
-    zone_id                = aws_api_gateway_domain_name.dns[each.value].cloudfront_zone_id
+    name                   = var.endpoint_type == "EDGE" ? aws_api_gateway_domain_name.dns[each.value].cloudfront_domain_name : aws_api_gateway_domain_name.dns[each.value].domain_name
+    zone_id                = var.endpoint_type == "EDGE" ? aws_api_gateway_domain_name.dns[each.value].cloudfront_zone_id : aws_api_gateway_domain_name.dns[each.value].zone_id
   }
 }
 

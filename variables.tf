459752f922a05f73734b52560a627652585f7745
Callum <callumccr@gmail.com>
2022-08-05 12:01:05 +0100

    added vpce for private endpoint types

--- variables.tf
+++ variables.tf
@@ -189,3 +189,9 @@ variable "description" {
   type        = string
   description = "Description of the REST API. If importing an OpenAPI specification via the body argument, this corresponds to the info.description field. If the argument value is provided and is different than the OpenAPI value, the argument value will override the OpenAPI value."
 }
+
+variable "vpc_endpoint_ids" {
+  default     = []
+  description = "Set of VPC Endpoint identifiers. It is only supported for PRIVATE endpoint type."
+  type        = list(any)
+}
